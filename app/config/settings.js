63478bd69896542327b604b9d70058ecd6332ffa
Ahmed Halim <ahmedhalim96@protonmail.com>
2021-01-22 01:02:39 +0200

    managing app settings different environment

---
+++ app/config/settings.js
@@ -0,0 +1,20 @@
+import Constants from "expo-constants";
+const settings = {
+	dev: {
+		apiUrl: "http://192.168.1.111:9001/api/",
+	},
+	staging: {
+		apiUrl: "http://192.168.1.111:9001/api/",
+	},
+	prod: {
+		apiUrl: "http://192.168.1.111:9001/api/",
+	},
+};
+
+const getCurrentSettings = () => {
+	if (__DEV__) return settings.dev;
+	if (Constants.manifest.releaseChannel === "staging") return settings.staging;
+	return settings.prod;
+};
+
+export default getCurrentSettings();
